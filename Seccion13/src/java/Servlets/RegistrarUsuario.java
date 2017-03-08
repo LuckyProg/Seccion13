1264395ee72d15c91a12e1c3dff23e0399a89111
Servín <tarzanboycraft@hotmail.com>
2017-03-08 13:28:51 -0600

    Servidor Adaptado
    add servlet tomcat javax exception

--- Seccion13/src/java/Servlets/RegistrarUsuario.java
+++ Seccion13/src/java/Servlets/RegistrarUsuario.java
@@ -5,7 +5,6 @@
  */
 package Servlets;
 
-import Clases.Usuario;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -15,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Fernando
+ * @author TarzanBoyCraft
  */
 public class RegistrarUsuario extends HttpServlet {
 
@@ -30,7 +29,19 @@ public class RegistrarUsuario extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Usuario usu = new Usuario();
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RegistrarUsuario</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RegistrarUsuario at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
